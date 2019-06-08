89a98b280a59eb3ea95d2ee96dc90d1deab8e29a
Matej Marko <matej.mbssk@gmail.com>
2019-06-08 03:54:17 +0200

    First commit

---
+++ app/src/main/java/com/example/mad_hangman/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mad_project
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.example.mad_hangman.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //var image: ImageView = findViewById(R.id.hangmanImage)
+        //image.setImageResource(R.drawable.i_6)
+    }
+}
