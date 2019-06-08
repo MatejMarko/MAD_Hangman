3feaaa42d68eebffe31469226bc3f9078534aee3
Matej Marko <matej.mbssk@gmail.com>
2019-06-08 05:12:36 +0200

    game design and initial word out print

--- app/src/main/java/com/example/mad_hangman/MainActivity.kt
+++ app/src/main/java/com/example/mad_hangman/MainActivity.kt
@@ -1,16 +1,41 @@
-package com.example.mad_project
+package com.example.mad_hangman
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
-import com.example.mad_hangman.R
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var word: String = "Zemljevid"
+    var guessingWord: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //var image: ImageView = findViewById(R.id.hangmanImage)
-        //image.setImageResource(R.drawable.i_6)
+        this.supportActionBar!!.hide()
+
+        var image: ImageView = findViewById(R.id.hangmanImage)
+        image.setImageResource(R.drawable.i_1)
+
+        guessingWord = findViewById(R.id.guessingWord)
+        setInitialWord()
+
+    }
+
+    fun letterPressed(v: View) {
+        var k: String = v.tag.toString()
+        guessingWord?.text = k
+    }
+
+    private fun setInitialWord() {
+        var dashedWord = ""
+        for (x in 0 until word.length-1) {
+            dashedWord += "_ "
+        }
+        dashedWord += "_"
+
+        guessingWord?.text = dashedWord
     }
 }
