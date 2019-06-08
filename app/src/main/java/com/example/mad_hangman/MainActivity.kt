ee5fd3bc9a036dac8399b04229e52f83b52aefb7
Matej Marko <matej.mbssk@gmail.com>
2019-06-08 15:34:19 +0200

    working version with static word

--- app/src/main/java/com/example/mad_hangman/MainActivity.kt
+++ app/src/main/java/com/example/mad_hangman/MainActivity.kt
@@ -1,26 +1,34 @@
 package com.example.mad_hangman
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.GridLayout
 import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    var word: String = "Zemljevid"
-    var guessingWord: TextView? = null
+    private var wordToGuess: String = "ZEMLJEVID"
+    private var wordToDisplay: String = "_________"
+    private var guessingWordTextView: TextView? = null //TODO can it be private
+    private var image: ImageView? = null
+    private var strikes = 0
+    private var maxStrikes = 6
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         this.supportActionBar!!.hide()
 
-        var image: ImageView = findViewById(R.id.hangmanImage)
-        image.setImageResource(R.drawable.i_1)
+        image = findViewById(R.id.hangmanImage)
+        image?.setImageResource(R.drawable.i_1_s)
 
-        guessingWord = findViewById(R.id.guessingWord)
-        setInitialWord()
+        guessingWordTextView = findViewById(R.id.guessingWord)
+        displayGuessingWord()
 
     }
 
@@ -28,24 +36,64 @@ class MainActivity : AppCompatActivity() {
     fun letterPressed(v: View) {
         var pressedLetter: Char = v.tag.toString()[0]
         var newWord = ""
-        for (x in 0 until word.length - 1) {
-            if (word[x] == pressedLetter && word[x] != '_') {
-                newWord += pressedLetter
-            } else {
-                newWord += "_ "
+        var wrongLetter = true
+        for (x in 0 until wordToDisplay.length) {
+            when {
+                wordToDisplay[x] != '_' -> newWord += wordToDisplay[x]
+                wordToGuess[x] == pressedLetter -> {
+                    newWord += pressedLetter
+                    wrongLetter = false
+                }
+                else -> {
+                    newWord += "_"
+                }
             }
         }
-        guessingWord?.text = newWord
-
+        if (wrongLetter) {
+            strikes++
+            changeHangmanPhoto()
+        }
+        wordToDisplay = newWord
+        displayGuessingWord()
+        handlePressedLetter(v.id, wrongLetter)
     }
 
-    private fun setInitialWord() {
+    private fun displayGuessingWord() {
         var dashedWord = ""
-        for (x in 0 until word.length-1) {
-            dashedWord += "_ "
+        for (x in 0 until wordToDisplay.length - 1) {
+            dashedWord += wordToDisplay[x].toString() + " "
         }
-        dashedWord += "_"
+        dashedWord += wordToDisplay[wordToDisplay.length - 1]
 
-        guessingWord?.text = dashedWord
+        guessingWordTextView?.text = dashedWord
+    }
+
+    private fun changeHangmanPhoto() {
+        when (strikes) {
+            1 -> image?.setImageResource(R.drawable.i_2_s)
+            2 -> image?.setImageResource(R.drawable.i_3_s)
+            3 -> image?.setImageResource(R.drawable.i_4_s)
+            4 -> image?.setImageResource(R.drawable.i_5_s)
+            5 -> image?.setImageResource(R.drawable.i_6_s)
+            6 -> image?.setImageResource(R.drawable.i_7_s)
+        }
+    }
+
+    private fun handlePressedLetter(letter: Int, wrong: Boolean) {
+        var b: Button = findViewById(letter)
+        if (!wrong)
+            b.setTextColor(Color.GREEN)
+        else
+            b.setTextColor(Color.RED)
+        b.isEnabled = false
+        if (strikes == maxStrikes) {
+            var displayedWord: TextView = findViewById(R.id.guessingWord)
+            displayedWord.setTextColor(Color.RED)
+            var letterLayout: GridLayout = findViewById(R.id.lettersGrid)
+            for (x in 0 until letterLayout.childCount) {
+                var child: Button = letterLayout.getChildAt(x) as Button
+                child.isEnabled = false
+            }
+        }
     }
 }
